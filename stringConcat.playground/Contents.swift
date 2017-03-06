ea75ea7fefb8da6a92d48510081671c7fade9c70
urian-mac <urianchang@gmail.com>
2017-03-06 10:27:08 -0800

    Added notes from learning platform

--- stringConcat.playground/Contents.swift
+++ stringConcat.playground/Contents.swift
@@ -9,3 +9,45 @@ var str = "Hello, playground"
 var name: String = "Anakin"
 //: Basic variable types: String, Int, Double
 
+//: CONDITIONALS
+//var rings = 5
+//if rings >= 5 {
+//    print ("You are welcome to join the party")
+//} else if rings > 2 {
+//    print ("Decent...but \(rings) rings aren't enough")
+//} else {
+//    print ("Go win some more rings")
+//}
+
+//: Integers
+//: 64 bits
+//print ("The maximum value \(Int.max)")
+//print ("The minimum value \(Int.min)")
+//: 32 bits (signed)
+//print("The maximum value \(Int32.max)")
+//print("The minimum value \(Int32.min)")
+//: 32 bits (unsigned)
+//print("The maximum value \(UInt32.max)")
+//print("The minimum value \(UInt32.min)")
+//: To convert... myNormalInt = Int(myInt32)
+//: While int's are precise, floating point types are not
+//: Double is the inferred type
+
+//: Conditionals
+//: For-in Loops
+//: A for-in loop performs a set of code in a specific range, sequence, or collection.
+// loop from 1 to 5 including 5
+//for i in 1...5 {
+//    print(i)
+//}
+// loop from 1 to 5 excluding 5
+//for i in 1..<5 {
+//    print(i)
+//}
+//: While loop
+var i = 1
+while i < 6 {
+    print(i)
+    i = i+1
+}
+
