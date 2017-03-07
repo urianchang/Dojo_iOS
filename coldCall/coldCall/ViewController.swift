c5818cd7034807a6019b7aac5cb2ea412e24634b
urian-mac <urianchang@gmail.com>
2017-03-07 12:04:40 -0800

    Completed Cold Call v2 assignment

--- coldCall/coldCall/ViewController.swift
+++ coldCall/coldCall/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var NameLabel: UILabel!
     
+    @IBOutlet weak var NumLabel: UILabel!
     
     @IBAction func ChangeButton(_ sender: Any) {
-        changeName()
+        changeUI()
     }
     
     let nameArr = ["TEST 1", "TEST 2", "TEST 3", "TEST 4", "TEST 5"]
@@ -22,12 +23,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        changeName()
+        NameLabel.text = "READY?"
+        NumLabel.isHidden = true
     }
 
-    func changeName() {
+    func changeUI() {
+        if NumLabel.isHidden {
+            NumLabel.isHidden = false
+        }
         let rand = Int(arc4random_uniform(UInt32(nameArr.count)))
         NameLabel.text = nameArr[rand]
+        NumLabel.text = String(Int(arc4random_uniform(UInt32(nameArr.count))) + 1)
+        if NumLabel.text == "1" || NumLabel.text == "2" {
+            NumLabel.textColor = UIColor.red
+        } else if NumLabel.text == "3" || NumLabel.text == "4" {
+            NumLabel.textColor = UIColor.orange
+        } else {
+            NumLabel.textColor = UIColor.green
+        }
+        
     }
     
     override func didReceiveMemoryWarning() {
