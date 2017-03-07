c18040b257fd7e837be186c8775b62cf8299ef27
urian-mac <urianchang@gmail.com>
2017-03-06 23:26:43 -0800

    Added Storyboards play v2

--- storyboardsPlayII/storyboardsPlayII/ViewController.swift
+++ storyboardsPlayII/storyboardsPlayII/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var poem = ["I want to play my storyboards",
+                "All over the Apple App store",
+                "And let my objects sing a song no one has heard before"]
+    var pageCount = 0
 
+    @IBOutlet weak var poemLabel: UILabel!
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        if pageCount < poem.count - 1 {
+            pageCount += 1
+        } else {
+            pageCount = 0
+        }
+        poemLabel.text = poem[pageCount]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        poemLabel.text = poem[pageCount]
     }
 
     override func didReceiveMemoryWarning() {
