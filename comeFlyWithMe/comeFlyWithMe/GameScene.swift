a9dd8dd46eb7b7d65995995287beac828c707fbc
urian-mac <urianchang@gmail.com>
2017-03-10 23:19:12 -0800

    Parallax background scrolling works

--- comeFlyWithMe/comeFlyWithMe/GameScene.swift
+++ comeFlyWithMe/comeFlyWithMe/GameScene.swift
@@ -11,100 +11,264 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    //: Variables that will be used
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // Sky
+    var skyNode : SKSpriteNode
+    var skyNodeNext : SKSpriteNode
     
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    // Clouds
+    var cloudsNode : SKSpriteNode
+    var cloudsNodeNext : SKSpriteNode
+    
+    // Ground
+    var groundNode : SKSpriteNode
+    var groundNodeNext : SKSpriteNode
+    
+    // Time of last frame
+    var lastFrameTime : TimeInterval = 0
+    
+    // Time since last frame
+    var deltaTime : TimeInterval = 0
+    
+    override init(size: CGSize) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        //: Check for current time
+        let current = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH"
+        let currentHour = formatter.string(from: current as Date)
+//        let currentHour = "8"     //Use for QA
+        print ("Current hour is \(currentHour)")
+        
+        //: Display appropriate sky background based on time
+        var bgname = String()
+        if Int(currentHour)! > 7 && Int(currentHour)! < 18 {
+            bgname = "daySky.png"
+        } else {
+            bgname = "nightSky.png"
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //: Prep sky layer
+        skyNode = SKSpriteNode(imageNamed: bgname)
+        skyNode.size = size
+        skyNode.position = CGPoint(x: size.width/2, y: size.height/2)
+//        skyNode.size = self.frame.size
+//        skyNode.position = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+        skyNodeNext = skyNode.copy() as! SKSpriteNode
+        skyNodeNext.position = CGPoint(x: skyNode.position.x + skyNode.size.width, y: skyNode.position.y)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        //: Prep cloud layer
+        cloudsNode = SKSpriteNode(imageNamed: "clouds.png")
+        cloudsNode.size = size
+        cloudsNode.position = CGPoint(x: size.width/2, y: size.height/2)
+//        cloudsNode.size = self.frame.size
+//        cloudsNode.position = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+        cloudsNodeNext = cloudsNode.copy() as! SKSpriteNode
+        cloudsNodeNext.position = CGPoint(x: cloudsNode.position.x + cloudsNode.size.width, y: cloudsNode.position.y)
+        
+        //: Prep ground layer
+        groundNode = SKSpriteNode(imageNamed: "ground.png")
+        groundNode.size = size
+        groundNode.position = CGPoint(x: size.width/2, y: size.height/2)
+//        groundNode.size = self.frame.size
+//        groundNode.position = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+        groundNodeNext = groundNode.copy() as! SKSpriteNode
+        groundNodeNext.position = CGPoint(x: groundNode.position.x + groundNode.size.width, y: groundNode.position.y)
+        
+        
+        super.init(size: size)
+        
+        //: Add background layers to the scene
+        self.addChild(skyNode)
+        self.addChild(skyNodeNext)
+        
+        self.addChild(cloudsNode)
+        self.addChild(cloudsNodeNext)
+        
+        self.addChild(groundNode)
+        self.addChild(groundNodeNext)
+        
+    }
+    
+    //: Borrowed code... I think this is a catch for in case init isn't run
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Not implemented")
+    }
+    
+    //: Helper function to move the background layers
+    func moveSprite(sprite : SKSpriteNode,
+                    nextSprite : SKSpriteNode, speed : Float) -> Void {
+        var newPosition = CGPoint.zero
+        
+        // For both the sprite and its duplicate:
+        for spriteToMove in [sprite, nextSprite] {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            // Shift the sprite leftward based on the speed
+            newPosition = spriteToMove.position
+            newPosition.x -= CGFloat(speed * Float(deltaTime))
+            spriteToMove.position = newPosition
+            
+            // If this sprite is now offscreen (i.e., its rightmost edge is
+            // farther left than the scene's leftmost edge):
+            if spriteToMove.frame.maxX < self.frame.minX {
+                
+                // Shift it over so that it's now to the immediate right
+                // of the other sprite.
+                // This means that the two sprites are effectively
+                // leap-frogging each other as they both move.
+                spriteToMove.position =
+                    CGPoint(x: spriteToMove.position.x +
+                        spriteToMove.size.width * 2,
+                            y: spriteToMove.position.y)
+            }
+            
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+    override func didMove(to view: SKView) {
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+//        //: Check for current time
+//        let current = NSDate()
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "HH"
+//        let currentHour = formatter.string(from: current as Date)
+//        print ("Current hour is \(currentHour)")
+//
+//        //: Display appropriate background based on time
+//        var bgname = String()
+//        if Int(currentHour)! > 7 && Int(currentHour)! < 18 {
+//            bgname = "daySky.png"
+//        } else {
+//            bgname = "nightSky.png"
+//        }
+//        let background = SKSpriteNode(imageNamed: bgname)
+//        background.size = self.frame.size
+//        background.position = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+//        addChild(background)
+
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        // First, update the delta time values:
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        // If we don't have a last frame time value, this is the first frame,
+        // so delta time will be zero.
+        if lastFrameTime <= 0 {
+            lastFrameTime = currentTime
         }
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
+        // Update delta time
+        deltaTime = currentTime - lastFrameTime
         
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
+        // Set last frame time to current time
+        lastFrameTime = currentTime
         
-        self.lastUpdateTime = currentTime
+        // Next, move each of the four pairs of sprites.
+        // Objects that should appear move slower than foreground objects.
+        self.moveSprite(sprite: skyNode, nextSprite:skyNodeNext, speed:25.0)
+        self.moveSprite(sprite: cloudsNode, nextSprite:cloudsNodeNext,
+                        speed:50.0)
+        self.moveSprite(sprite: groundNode, nextSprite:groundNodeNext, speed:75.0)
     }
 }
+
+//class GameScene: SKScene {
+//    
+//    var entities = [GKEntity]()
+//    var graphs = [String : GKGraph]()
+//    
+//    private var lastUpdateTime : TimeInterval = 0
+//    private var label : SKLabelNode?
+//    private var spinnyNode : SKShapeNode?
+//    
+//    override func sceneDidLoad() {
+//
+//        self.lastUpdateTime = 0
+//        
+//        // Get label node from scene and store it for use later
+//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+//        if let label = self.label {
+//            label.alpha = 0.0
+//            label.run(SKAction.fadeIn(withDuration: 2.0))
+//        }
+//        
+//        // Create shape node to use during mouse interaction
+//        let w = (self.size.width + self.size.height) * 0.05
+//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//        
+//        if let spinnyNode = self.spinnyNode {
+//            spinnyNode.lineWidth = 2.5
+//            
+//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
+//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+//                                              SKAction.fadeOut(withDuration: 0.5),
+//                                              SKAction.removeFromParent()]))
+//        }
+//    }
+//    
+//    
+//    func touchDown(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.green
+//            self.addChild(n)
+//        }
+//    }
+//    
+//    func touchMoved(toPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.blue
+//            self.addChild(n)
+//        }
+//    }
+//    
+//    func touchUp(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.red
+//            self.addChild(n)
+//        }
+//    }
+//    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if let label = self.label {
+//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
+//        
+//        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+//    }
+//    
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+//    }
+//    
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//    }
+//    
+//    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//    }
+//    
+//    
+//    override func update(_ currentTime: TimeInterval) {
+//        // Called before each frame is rendered
+//        
+//        // Initialize _lastUpdateTime if it has not already been
+//        if (self.lastUpdateTime == 0) {
+//            self.lastUpdateTime = currentTime
+//        }
+//        
+//        // Calculate time since last update
+//        let dt = currentTime - self.lastUpdateTime
+//        
+//        // Update entities
+//        for entity in self.entities {
+//            entity.update(deltaTime: dt)
+//        }
+//        
+//        self.lastUpdateTime = currentTime
+//    }
+//}
