478183f47efb0a097812cfecab9a5642d7649fea
urian-mac <urianchang@gmail.com>
2017-03-13 22:14:29 -0700

    Completed Adding to Beast List assignment

--- beastList/beastList/ViewController.swift
+++ beastList/beastList/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tasks = ["Something cool", "Something very cool", "Something extremely cool"]
+    
     @IBOutlet weak var taskTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
     @IBAction func beastButtonPressed(_ sender: UIButton) {
-        
+        tasks.append(taskTextField.text!)
+        tableView.reloadData()
+        taskTextField.text = ""
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,3 +35,19 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    //: How many cells
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    //: How to create each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
+}
