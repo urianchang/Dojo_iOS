3cf95aa25e901c32a09972715936275bb0fa5a5b
urian-mac <urianchang@gmail.com>
2017-03-14 11:28:33 -0700

    Created example app to test modal segues

---
+++ modalSceneTest/modalSceneTest/OtherViewController.swift
@@ -0,0 +1,38 @@
+//
+//  OtherViewController.swift
+//  modalSceneTest
+//
+//  Created by Urian Chang on 3/14/17.
+//  Copyright © 2017 CodingDojo. All rights reserved.
+//
+
+import UIKit
+
+class OtherViewController: UIViewController {
+    @IBAction func dismissButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
