5c78be09dc44e259525497b8926b8c2ec7388841
urian-mac <urianchang@gmail.com>
2017-03-20 15:33:25 -0700

    Added Bucket List v2 to implement Core Data

---
+++ tableViewControllerTest2/tableViewControllerTest/BucketListViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  tableViewControllerTest
+//
+//  Created by Urian Chang on 3/14/17.
+//  Copyright © 2017 CodingDojo. All rights reserved.
+//
+
+//: Going to implement Core Data in this version!!
+
+import UIKit
+
+class BucketListViewController: UITableViewController, AddItemTableViewControllerDelegate {
+    var items = ["Go to Moon", "Eat a candybar", "Swim in the Amazon", "Ride a motorbike in Tokyo"]
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "EditItemSegue", sender: sender)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listItemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        performSegue(withIdentifier: "EditItemSegue", sender: indexPath)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print ("row selected")
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        //: Default is the delete functionality
+        items.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+//        if segue.identifier == "AddItemSegue" {
+//            print (sender)
+//            let navigationController = segue.destination as! UINavigationController
+//            let addItemTableViewController = navigationController.topViewController as! addItemTableViewController
+//            addItemTableViewController.delegate = self
+//        } else if segue.identifier == "EditItemSegue" {
+//            print (sender)
+//            let navigationController = segue.destination as! UINavigationController
+//            let addItemTableViewController = navigationController.topViewController as! addItemTableViewController
+//            addItemTableViewController.delegate = self
+//            let indexPath = sender as! NSIndexPath
+//            let item = items[indexPath.row]
+//            addItemTableViewController.item = item
+//            addItemTableViewController.indexPath = indexPath
+//        }
+
+        let navigationController = segue.destination as! UINavigationController
+        let addItemTableViewController = navigationController.topViewController as! addItemTableViewController
+        addItemTableViewController.delegate = self
+        
+        if let senderobj = sender as? NSIndexPath {
+            let item = items[senderobj.row]
+            addItemTableViewController.item = item
+            addItemTableViewController.indexPath = senderobj
+        }
+
+    }
+    
+    func cancelButtonPressed(by controller: addItemTableViewController) {
+//        print ("I am the hidden controller, but I am responding to the cancel button press on the top view controller.")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func itemSaved(by controller: addItemTableViewController, with text: String, at indexPath: NSIndexPath?) {
+//        print ("Received text from top view: \(text)")
+        
+        if let ip = indexPath {
+            items[ip.row] = text
+        } else {
+            items.append(text)
+        }
+        tableView.reloadData()
+        dismiss(animated: true, completion: nil)
+    }
+}
+
---
+++ tableViewControllerTest2/tableViewControllerTest/addItemTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  addItemTableViewController.swift
+//  tableViewControllerTest
+//
+//  Created by Urian Chang on 3/14/17.
+//  Copyright © 2017 CodingDojo. All rights reserved.
+//
+
+import UIKit
+
+class addItemTableViewController: UITableViewController {
+
+    weak var delegate : AddItemTableViewControllerDelegate?
+    
+    var item : String?
+    var indexPath : NSIndexPath?
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        delegate?.cancelButtonPressed(by: self)
+    }
+    
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        let text = itemTextField.text!
+        delegate?.itemSaved(by: self, with: text, at: indexPath)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        itemTextField.text = item
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    //: Don't need for static cells
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 1
+//    }
+
+
+}
