f23c41dd6e9acf25e397ad28573f88842197bdf8
urian-mac <urianchang@gmail.com>
2017-03-20 23:47:37 -0700

    Completed Binary Counter assignment

--- binaryCounter/binaryCounter/CustomCell.swift
+++ binaryCounter/binaryCounter/CustomCell.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+protocol CustomCellButtonsDelegate : class {
+    func addTapped(cell: CustomCell)
+    func subTapped(cell: CustomCell)
+}
+
+
+class CustomCell: UITableViewCell {
+    
+    var buttonDelegate : CustomCellButtonsDelegate?
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBAction func subtractPressed(_ sender: UIButton) {
+        buttonDelegate?.subTapped(cell: self)
+    }
+    
+    @IBAction func addPressed(_ sender: UIButton) {
+        buttonDelegate?.addTapped(cell: self)
+    }
+}
--- binaryCounter/binaryCounter/ViewController.swift
+++ binaryCounter/binaryCounter/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, CustomCellButtonsDelegate {
 
+    var total : Int = 0
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        totalLabel.text = "Total: \(total)"
     }
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //: Return an int that indicates how many rows (cells) to draw
+        return 16
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //: Get the UITableViewCell and create/populate it with data then return it
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomCell
+        let val = Int(pow(10.0, Double(indexPath.row)))
+        cell.valueLabel.text = "\(val)"
+        
+        //: Add button delegate for button actions
+        cell.buttonDelegate = self
+        
+        return cell
+    }
+    
+    //: Custom Cell Buttons
+    func addTapped(cell: CustomCell) {
+        total = total + Int(cell.valueLabel.text!)!
+        totalLabel.text = "Total: \(total)"
+    }
+    
+    func subTapped(cell: CustomCell) {
+        total = total - Int(cell.valueLabel.text!)!
+        totalLabel.text = "Total: \(total)"
+    }
 }
 
