105009ae16cd0fcafbd703956976bdbe91a797fa
urian-mac <urianchang@gmail.com>
2017-03-20 17:06:28 -0700

    Added Custom Cell example from learning platform

---
+++ customCell/CustomCellsTableViewController.swift
@@ -0,0 +1,96 @@
+//
+//  CustomCellsTableViewController.swift
+//  customCell
+//
+//  Created by Urian Chang on 3/20/17.
+//  Copyright © 2017 CodingDojo. All rights reserved.
+//
+
+import UIKit
+
+class CustomCellsTableViewController: UITableViewController {
+
+    var nums = [1, 90, 32, 23, 9, 12]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return nums.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomCell
+        cell.rightLabel.text = "\(nums[indexPath.row])"
+        if nums[indexPath.row] > 24 {
+            cell.leftButton.backgroundColor = UIColor.green
+        } else {
+            cell.leftButton.backgroundColor = UIColor.red
+        }
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ customCell/customCell/CustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCell.swift
+//  customCell
+//
+//  Created by Urian Chang on 3/20/17.
+//  Copyright © 2017 CodingDojo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightLabel: UILabel!
+    
+}
