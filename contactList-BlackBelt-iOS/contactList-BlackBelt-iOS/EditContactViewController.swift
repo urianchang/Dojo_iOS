842f4f7948c222493f4c4f3428ab9b0c8acd31a5
urian-mac <urianchang@gmail.com>
2017-03-24 09:30:45 -0700

    Main UI laid out

---
+++ contactList-BlackBelt-iOS/contactList-BlackBelt-iOS/EditContactViewController.swift
@@ -0,0 +1,52 @@
+//
+//  EditContactViewController.swift
+//  contactList-BlackBelt-iOS
+//
+//  Created by Urian Chang on 3/24/17.
+//  Copyright © 2017 CodingDojo. All rights reserved.
+//
+
+import UIKit
+
+class EditContactViewController: UIViewController {
+
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    
+    
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
